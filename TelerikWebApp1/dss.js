3487d724f7f5e9edb5e3ef0f49533fef02ed95aa
shirat <shirat@safeguard.co.il>
2017-09-28 17:27:36 +0300

    to do to arrays of urlimg 1 anf two to one array

---
+++ TelerikWebApp1/dss.js
@@ -0,0 +1,49 @@
+﻿var dss = (function (dss) {
+    'use strict';
+
+    var getObject = function (values, columns) {
+        var obj = {};
+
+        for (var c = 0; c < columns.length; c++) {
+            obj[columns[c]] = values[c];
+        }
+
+        return obj;
+    };
+
+    var transformQueryResults = function (records, columns) {
+        var data = [];
+
+        if (records) {
+            for (var i = 0; i < records.length; i++) {
+                var obj = getObject(records[i], columns);
+                data.push(obj);
+            }
+            return data;
+        }
+    };
+
+    return {
+        transformRecords: function (records, columns) {
+            return transformQueryResults(records, columns);
+        },
+
+        getRelatedRows: function (objName, id, relName, columns) {
+            return $.Deferred(function () {
+                var self = this;
+
+                rbf_getRelatedIds(relName, id, function (relName, id, values) {
+                    //var query = 'SELECT ' + columns.join(',') + ' FROM ' + objName + ' WHERE cb_isActive= 1 and id IN (' + values.join(',') + ')';
+                    var query = 'SELECT ' + columns.join(',') + ' FROM ' + objName + ' WHERE id IN (' + values.join(',') + ')';
+
+                    rbf_selectQuery(query, 200, function (result) {
+                        var data = transformQueryResults(result, columns);
+
+                        self.resolve(data);
+                    });
+
+                })
+            });
+        }
+    }
+}(this.dss = this.dss || {}));
